ca55f08b9bdcb0aa939e48ddb1c792f10574bbc6
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-04-07 21:41:28 -0300

    feat: add header

--- ts/src/app/header/header.component.ts
+++ ts/src/app/header/header.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  headerData = {
+    title: 'Anais do Simpósio Latino Americano de Ciências de Alimentos',
+    description:
+      'Anais do 13° Simpósio Latino Americano de Ciências de Alimentos',
+    issin: '1234-5678',
+  };
+  profileData = {
+    email: 'alguem12@galoascience.com',
+    image: 'assets/profile-image.png',
+  };
 }
