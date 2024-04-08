1b199d66b540d41fb3fcb20fff23f38187881a9e
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-04-08 19:37:19 -0300

    feat: add button component

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { MainDetailsComponent } from './main-details/main-details.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, HeaderComponent, MainComponent],
+  declarations: [AppComponent, SideBarComponent, HeaderComponent, MainComponent, MainDetailsComponent, ButtonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ ts/src/app/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() buttonIcon = '';
+  @Input() buttonAlt = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
