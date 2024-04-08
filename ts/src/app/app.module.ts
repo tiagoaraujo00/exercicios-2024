c6c9a968798e22fbdf0a2cacfa3f847d40f3ae10
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-04-08 19:13:17 -0300

    feat: add video wrapper component

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, HeaderComponent],
+  declarations: [AppComponent, SideBarComponent, HeaderComponent, MainComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
